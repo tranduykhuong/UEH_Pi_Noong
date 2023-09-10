d9d14e86f9e9630c3906362b7bf717a89f093aec
joseph <tranduykhuongit@gmail.com>
2023-09-10 17:20:39 +0700

    test geolocation

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -275,10 +275,10 @@ const mapStyles = {
 const Home = () => {
     const [currentPosition, setCurrentPosition] = useState(null);
     const [watchId, setWatchId] = useState(null);
-    // const [targetLatitude, setTargetLatitude] = useState(10.766894); // Thay thế bằng tọa độ GPS của mục tiêu
-    // const [targetLongitude, setTargetLongitude] = useState(106.695466);
-    const [targetLatitude, setTargetLatitude] = useState(10.766970623687978);
-    const [targetLongitude, setTargetLongitude] = useState(106.69504968618132);
+    const [targetLatitude, setTargetLatitude] = useState(10.766894); // Thay thế bằng tọa độ GPS của mục tiêu
+    const [targetLongitude, setTargetLongitude] = useState(106.695466);
+    // const [targetLatitude, setTargetLatitude] = useState(10.766970623687978);
+    // const [targetLongitude, setTargetLongitude] = useState(106.69504968618132);
     // const [targetLatitude, setTargetLatitude] = useState(10.766703394544189);
     // const [targetLongitude, setTargetLongitude] = useState(106.69524844454077);
     // const [targetLatitude, setTargetLatitude] = useState(10.766736589044667);
