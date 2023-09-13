25d942ba6c2b1142d29e88408cb8b08c86fdfd3a
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-09-14 00:38:52 +0700

    TV: getCurrentPosition

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -61,23 +61,23 @@ const Home = () => {
     // }, []);
 
     const startWatchingHeading = () => {
-        if ('ondeviceorientation' in window) {
-            watchId = navigator.compass.watchHeading(
-                (heading) => {
-                    // Xử lý thay đổi hướng ở đây
-                    const magneticHeading = heading.magneticHeading;
-                    const trueHeading = heading.trueHeading;
-                    console.log('Hướng từ cảm biến la bàn (Magnetic):', magneticHeading);
-                    console.log('Hướng từ cảm biến la bàn (True):', trueHeading);
-                    setHeading(magneticHeading);
-                },
-                (error) => {
-                    console.error('Lỗi khi theo dõi hướng:', error);
-                }
-            );
-        } else {
-            console.log('Trình duyệt không hỗ trợ cảm biến la bàn.');
-        }
+        // if ('ondeviceorientation' in window) {
+        watchId = navigator.compass.watchHeading(
+            (heading) => {
+                // Xử lý thay đổi hướng ở đây
+                const magneticHeading = heading.magneticHeading;
+                const trueHeading = heading.trueHeading;
+                console.log('Hướng từ cảm biến la bàn (Magnetic):', magneticHeading);
+                console.log('Hướng từ cảm biến la bàn (True):', trueHeading);
+                setHeading(magneticHeading);
+            },
+            (error) => {
+                console.error('Lỗi khi theo dõi hướng:', error);
+            }
+        );
+        // } else {
+        //     console.log('Trình duyệt không hỗ trợ cảm biến la bàn.');
+        // }
     };
 
     const calculateDistance = (lat1, lon1, lat2, lon2) => {
