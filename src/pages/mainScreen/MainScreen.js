a7f5f380ab57d0bef03e10973341e52a8466e767
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-10-02 12:11:07 +0700

    TV: fix

--- src/pages/mainScreen/MainScreen.js
+++ src/pages/mainScreen/MainScreen.js
@@ -184,7 +184,7 @@ const MainScreen = () => {
                 for (let i = 0; i < data.length; i++) {
                     const element = data[i];
                     const dist = calculateDistance(currentPosition.lat, currentPosition.lng, element.lat, element.lng);
-                    if (dist < 0.01 && !yieldModal) {
+                    if (dist < 0.005 && !yieldModal) {
                         setYield(element);
                         setYieldModal(true);
 
